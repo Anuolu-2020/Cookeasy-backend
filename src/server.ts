10c73486fed0adfe6d4b0b3bba83f15b827abc74
Ogunleye Emmanuel <Anuolu1645@gmail.com>
2023-12-14 10:54:26 +0100

    chore: connected to database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import { app } from "./app";
 
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING as string)
+  .then(() => {
+    console.log("Connected Successfully to Database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const PORT = process.env.PORT || 3000;
 
