202caefdff8fd23f479d0858d97dcde7de7c856b
Ogunleye Emmanuel <Anuolu1645@gmail.com>
2023-12-14 12:29:48 +0100

    feat: added user route

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,14 @@ import {
   checkApiVersion,
 } from "./middlewares/checkApiVersion.middleware";
 import { logger } from "./utils/logging";
-
+import { userRoute } from "./routes/users.route";
 export const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(logger());
+app.use(logger()); // logging
 
 app.use("/api/v:version/", checkApiVersion);
+app.use("/api/v:version/users", userRoute); //user routes
 
-app.get("/api/v:version/", apiWelcome);
+app.get("/api/v:version/", apiWelcome); // Api home endpoint
